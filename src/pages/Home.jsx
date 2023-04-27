dc84461cda71240ece4d7536c8678b2d72624c07
Kartik Gupta <kartikgupta7267@gmail.com>
2023-04-27 21:55:21 +0530

    sss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ export default function FullWidthGrid() {
                 Kartik Gupta
               </Typography>
               <Typography level="h5" backgroundColor={'var(--onyx)'}>
-                Web Developer
+                Web Developer 
               </Typography>
             </Grid>
 
