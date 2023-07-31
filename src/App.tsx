4095858e35d61a8b2dd349f9dd89070c33b6cf0b
Kartik Gupta <kartikgupta7267@gmail.com>
2023-08-01 03:01:20 +0530

    Intail Build with dark mode

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import Home from './pages/Home';
+
+function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <Home />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
+
+export default App
