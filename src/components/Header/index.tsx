62a9f1e7d7bbf8a44163d59b0f80eb87a3d28c53
Kartik Gupta <kartikgupta7267@gmail.com>
2023-08-02 17:10:15 +0530

    navbar added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import {
+  createStyles,
+  Header as MantineHeader,
+  Group,
+  ActionIcon,
+  Container,
+  Burger,
+  rem,
+  useMantineColorScheme,
+  Text,
+  Image,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+  IconSun,
+  IconMoonStars,
+} from "@tabler/icons-react";
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: rem(56),
+
+    [theme.fn.smallerThan("sm")]: {
+      justifyContent: "flex-start",
+    },
+  },
+
+  links: {
+    width: rem(260),
+
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  social: {
+    width: rem(260),
+
+    [theme.fn.smallerThan("sm")]: {
+      width: "auto",
+      marginLeft: "auto",
+    },
+  },
+
+  burger: {
+    marginRight: theme.spacing.md,
+
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkActive: {
+    "&, &:hover": {
+      backgroundColor: theme.fn.variant({
+        variant: "light",
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+        .color,
+    },
+  },
+}));
+
+const links =  [
+  {
+    "link": "/about",
+    "label": "Home"
+  },
+  {
+    "link": "/learn",
+    "label": "Features"
+  },
+  {
+    "link": "/pricing",
+    "label": "Pricing"
+  }
+];
+
+export default function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={cx(classes.link, {
+        [classes.linkActive]: active === link.link,
+      })}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <MantineHeader height={56} mb={120} withBorder>
+      <Container className={classes.inner}>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          size="sm"
+          className={classes.burger}
+        />
+
+        <Group className={classes.links} spacing={5}>
+          <Image maw={60} src={"./handy-meditating-cloud.gif"} />
+
+           <Text size="lg" weight={700} color={dark ? "lightGray" : "black"}>
+             @kartik-gupta-ij
+          </Text>
+        </Group>
+
+        <Group spacing={15} className={classes.social} position="right" noWrap>
+          <ActionIcon variant="light" color="blue">
+            <IconBrandTwitter size="1.1rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon variant="light" color="blue">
+            <IconBrandYoutube size="1.1rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon variant="light" color="blue">
+            <IconBrandInstagram size="1.1rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon
+            variant="light"
+            color={dark ? "yellow" : "blue"}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+          </ActionIcon>
+        </Group>
+      </Container>
+    </MantineHeader>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import IntroSection from "@/components/IntroSection";
+
+
+
+
+export default function Home() {
+    return (
+     <>
+      <Header  />
+      <IntroSection/>
+      </>
+    );
+  }
